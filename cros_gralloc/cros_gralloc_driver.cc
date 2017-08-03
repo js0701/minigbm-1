intel/minigbm

AndroidIA: Replace pthread locks with spin lock.
Seems to make system more responsive and recover from
crashes.

Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

cros_gralloc/cros_gralloc_driver.cc
@@ -148,7 +148,7 @@ int32_t cros_gralloc_driver::allocate(const struct cros_gralloc_buffer_descripto
 	id = drv_bo_get_plane_handle(bo, 0).u32;
 	auto buffer = new cros_gralloc_buffer(id, bo, hnd);
 
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 	_buffers[id] = buffer;
 	_handles[hnd] = std::make_pair(buffer, 1);
 	*out_handle = &hnd->base;
@@ -158,7 +158,7 @@ int32_t cros_gralloc_driver::allocate(const struct cros_gralloc_buffer_descripto
 int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 {
 	uint32_t id;
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -214,7 +214,7 @@ int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 
 int32_t cros_gralloc_driver::release(buffer_handle_t handle)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -242,7 +242,7 @@ int32_t cros_gralloc_driver::release(buffer_handle_t handle)
 int32_t cros_gralloc_driver::map(buffer_handle_t handle, int32_t acquire_fence, uint64_t flags,
 				 void *addr[DRV_MAX_PLANES])
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -265,7 +265,7 @@ int32_t cros_gralloc_driver::map(buffer_handle_t handle, int32_t acquire_fence, 
 
 int32_t cros_gralloc_driver::unmap(buffer_handle_t handle, int32_t *release_fence)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -285,7 +285,7 @@ int32_t cros_gralloc_driver::unmap(buffer_handle_t handle, int32_t *release_fenc
 
 int32_t cros_gralloc_driver::get_backing_store(buffer_handle_t handle, uint64_t *out_store)
 {
-	std::lock_guard<std::mutex> lock(_mutex);
+	SCOPED_SPIN_LOCK(_mutex);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {

cros_gralloc/cros_gralloc_driver.h
@@ -9,8 +9,9 @@
 
 #include "cros_gralloc_buffer.h"
 
-#include <mutex>
 #include <unordered_map>
+
+#include "cros_gralloc_spinlock.h"
 
 class cros_gralloc_driver
 {
@@ -38,7 +39,7 @@ class cros_gralloc_driver
 	cros_gralloc_buffer *get_buffer(cros_gralloc_handle_t hnd);
 
 	struct driver *_drv;
-	std::mutex _mutex;
+	SpinLock _mutex;
 	std::unordered_map<uint32_t, cros_gralloc_buffer *> _buffers;
 	std::unordered_map<cros_gralloc_handle_t, std::pair<cros_gralloc_buffer *, int32_t>>
 	    _handles;

js0701/minigbm-1

cros_gralloc/cros_gralloc_driver.cc
@@ -148,7 +148,7 @@ int32_t cros_gralloc_driver::allocate(const struct cros_gralloc_buffer_descripto
 	id = drv_bo_get_plane_handle(bo, 0).u32;
 	auto buffer = new cros_gralloc_buffer(id, bo, hnd);
 
-	std::lock_guard<std::mutex> lock(mutex_);
+        SCOPED_SPIN_LOCK(mutex_);
 	buffers_.emplace(id, buffer);
 	handles_.emplace(hnd, std::make_pair(buffer, 1));
 	*out_handle = &hnd->base;
@@ -158,7 +158,7 @@ int32_t cros_gralloc_driver::allocate(const struct cros_gralloc_buffer_descripto
 int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 {
 	uint32_t id;
-	std::lock_guard<std::mutex> lock(mutex_);
+        SCOPED_SPIN_LOCK(mutex_);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -214,7 +214,7 @@ int32_t cros_gralloc_driver::retain(buffer_handle_t handle)
 
 int32_t cros_gralloc_driver::release(buffer_handle_t handle)
 {
-	std::lock_guard<std::mutex> lock(mutex_);
+        SCOPED_SPIN_LOCK(mutex_);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -246,7 +246,7 @@ int32_t cros_gralloc_driver::lock(buffer_handle_t handle, int32_t acquire_fence,
 	if (ret)
 		return ret;
 
-	std::lock_guard<std::mutex> lock(mutex_);
+	SCOPED_SPIN_LOCK(mutex_);
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
 		cros_gralloc_error("Invalid handle.");
@@ -264,7 +264,7 @@ int32_t cros_gralloc_driver::lock(buffer_handle_t handle, int32_t acquire_fence,
 
 int32_t cros_gralloc_driver::unlock(buffer_handle_t handle, int32_t *release_fence)
 {
-	std::lock_guard<std::mutex> lock(mutex_);
+        SCOPED_SPIN_LOCK(mutex_);;
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {
@@ -290,7 +290,7 @@ int32_t cros_gralloc_driver::unlock(buffer_handle_t handle, int32_t *release_fen
 
 int32_t cros_gralloc_driver::get_backing_store(buffer_handle_t handle, uint64_t *out_store)
 {
-	std::lock_guard<std::mutex> lock(mutex_);
+        SCOPED_SPIN_LOCK(mutex_);
 
 	auto hnd = cros_gralloc_convert_handle(handle);
 	if (!hnd) {

cros_gralloc/cros_gralloc_driver.h
@@ -9,8 +9,9 @@
 
 #include "cros_gralloc_buffer.h"
 
-#include <mutex>
 #include <unordered_map>
+
+#include "cros_gralloc_spinlock.h"
 
 class cros_gralloc_driver
 {
@@ -38,7 +39,7 @@ class cros_gralloc_driver
 	cros_gralloc_buffer *get_buffer(cros_gralloc_handle_t hnd);
 
 	struct driver *drv_;
-	std::mutex mutex_;
+        SpinLock mutex_;
 	std::unordered_map<uint32_t, cros_gralloc_buffer *> buffers_;
 	std::unordered_map<cros_gralloc_handle_t, std::pair<cros_gralloc_buffer *, int32_t>>
 	    handles_;