intel/minigbm

ANDROIDIA: Add Gralloc1.0 support
This is disabled for now.

Jira: None.
Test: Build passes on Android.

Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

Android.gralloc.mk
@@ -8,4 +8,9 @@ LOCAL_SRC_FILES += \
 	cros_gralloc/cros_gralloc_buffer.cc \
 	cros_gralloc/cros_gralloc_driver.cc \
 	cros_gralloc/cros_gralloc_helpers.cc \
-	cros_gralloc/gralloc0/cros_gralloc_module.cc
+
+ifeq ($(strip $(BOARD_USES_GRALLOC1)), true)
+LOCAL_SRC_FILES += cros_gralloc/gralloc1/cros_gralloc1_module.cc
+else
+LOCAL_SRC_FILES += cros_gralloc/gralloc0/cros_gralloc_module.cc
+endif

js0701/minigbm-1

Signed-off-by: Munish Bhardwaj <munishx.bhardwaj@intel.com>
Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>
Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

Android.gralloc.mk
@@ -7,5 +7,10 @@ LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES += \
 	cros_gralloc/cros_gralloc_buffer.cc \
 	cros_gralloc/cros_gralloc_driver.cc \
-	cros_gralloc/cros_gralloc_helpers.cc \
-	cros_gralloc/gralloc0/gralloc0.cc
+        cros_gralloc/cros_gralloc_helpers.cc
+
+ifeq ($(strip $(BOARD_USES_GRALLOC1)), true)
+LOCAL_SRC_FILES += cros_gralloc/gralloc1/cros_gralloc1_module.cc
+else
+LOCAL_SRC_FILES += cros_gralloc/gralloc0/gralloc0.cc
+endif